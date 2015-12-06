3cc72e65549f1dd3456a53d6f0a6a8164a7c2f5d
fourohfour <fourohfourbukkitdev@gmail.com>
2015-12-06 10:43:16 +0000

    First Commit V.1.0
    V. 1.0

---
+++ readit.py
@@ -0,0 +1,61 @@
+from flask import Flask, url_for
+import praw
+app = Flask(__name__)
+
+subs = ['Formula1', 'WEC', 'MechanicalKeyboards', 'BTCC', 'FountainPens', 'Programming', 'ProgrammerHumor', 'Civ', 'TruckSim', 'FlightSim', 'Flying', 'AskReddit', 'WorldNews', 'All']
+r = praw.Reddit(user_agent="Readit: Reddit as a newsstream viewer")
+
+@app.route('/r/<sub>')
+def readSub(sub):
+    starthtml = """<html><head>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu">
+    </head><body><div style=\"font-family:Ubuntu\">"""
+    subreddit = r.get_subreddit(sub)
+    starthtml += "<div style=\"background-color: PowderBlue;padding-top:10px;height:50px\">"
+    starthtml += "<span style=\"font-weight:bold; width:200px;float:left;padding-left:10px\">" + sub + "</span>"
+    starthtml += "<span style=\"width:calc(100% - 300px);text-align:right;float:right;padding-right:10px\">" + ("&nbsp;" * 5).join(["<a href = '/r/" + s + "'>" + s + "</a>" for s in subs]) + "</span>"
+    starthtml += "</div>"
+    colours = ["floralwhite", "lightgrey"]
+    colourpick = False
+    for submission in subreddit.get_top_from_day(limit=20):
+        tags = ""
+        addto = False
+        newtitle = list(submission.title)
+        for i, c in enumerate(submission.title):
+            if addto:
+                newtitle[i] = ""
+                if c == "]":
+                    addto = False
+                    tags = tags + ", "
+                    continue
+                tags = tags + c
+            else:
+                if c == "[":
+                    addto = True
+                    newtitle[i] = ""
+        taggedas = ""
+        if tags:
+            tags = tags[:-2]
+
+        is_self = submission.is_self
+        taggedas = "<div style=\"text-align:center;width:100px;height:50px;vertical-align:center;line-height:50px\">" + tags + "</div>"
+
+        starthtml += "<div style=\"padding-top:10px;padding-bottom:10px;width:100%;overflow: auto;background-color: " + colours[int(colourpick)] + "\">"
+        starthtml += "<div style=\"width:100px; height:50px; float:left; background-image:url('" + url_for("static", filename = ((is_self * "selfpost.png") + (not is_self) * "linkpost.png")) + "')\">" + taggedas + "</div>"
+        starthtml += "<div style=\"font-size:18px;float:left;width: calc(100% - 100px)\"><a href=\"" + submission.url + "\" target=\"_blank\">" + "".join(newtitle) + "</a>"
+        starthtml += "</br>""<i>Submitted by " + submission.author.name + " with " + str(submission.score) + " points." + " <a href=\"" + submission.short_link + "\" target=\"_blank\">[comments]</a></i></div></div>"
+        colourpick = not colourpick
+    endhtml = "</div></body></html>"
+    return starthtml + endhtml
+        
+@app.route('/')
+def mainPage():
+    starthtml = "<html><body>"
+    endhtml = "</body></html>"
+    return starthtml + endhtml
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.81')
+    r.login('readit_crawler', 'readit')
+    
